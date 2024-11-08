9caf7f549e53f9c6ed82f1276bbfaf6bdb8a85fb
Piotr Furmanek <info.pete85@gmail.com>
2024-11-08 16:03:37 +0000

    build script

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
-const livereload = require('gulp-livereload');
-const { src, dest, watch, series } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const terser = require('gulp-terser');
@@ -12,53 +11,62 @@ const browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
+// Compile SCSS into CSS, minify with cssnano, and output with sourcemaps
 const scssTask = () => {
   return src('styles/styles.scss', { sourcemaps: true })
-    .pipe(sass({}, {}))
-    .pipe(postcss([cssnano()]))
-    .pipe(dest('dist', { sourcemaps: '.' }));
+      .pipe(sass().on('error', sass.logError))
+      .pipe(postcss([cssnano()]))
+      .pipe(dest('dist', { sourcemaps: '.' }))
+      .pipe(browserSync.stream());
 };
 
+// Minify JavaScript and output with sourcemaps
 const jsTask = () => {
   return src('scripts/hex.js', { sourcemaps: true })
-    .pipe(terser())
-    .pipe(dest('dist', { sourcemaps: '.' }));
+      .pipe(terser())
+      .pipe(dest('dist', { sourcemaps: '.' }))
+      .pipe(browserSync.stream());
 };
 
+// Copy HTML files to dist folder
+const htmlTask = () => {
+  return src('*.html')
+      .pipe(dest('dist'))
+      .pipe(browserSync.stream());
+};
+
+// Serve files with BrowserSync
 const browserSyncServe = (cb) => {
   browserSync.init({
     server: {
-      baseDir: '.'
+      baseDir: 'dist'
     }
   });
   cb();
-}
+};
 
+// Reload BrowserSync
 const browserSyncReload = (cb) => {
   browserSync.reload();
   cb();
-}
+};
 
-gulp.task('watch', function () {
-  gulp.watch('./styles/*.scss', gulp.series('scss'));
-  gulp.watch('./scripts*.js', gulp.series('js'));
-  livereload.listen({ host: 'localhost' });
-});
-
+// Watch files for changes and trigger appropriate tasks
 const watchTask = () => {
-  watch('*.html', browserSyncReload);
-  watch(['./styles/**/*.scss', './scripts/**/*.js'], series(scssTask, jsTask, browserSyncReload));
-}
+  watch('*.html', series(htmlTask, browserSyncReload));
+  watch('./styles/**/*.scss', series(scssTask, browserSyncReload));
+  watch('./scripts/**/*.js', series(jsTask, browserSyncReload));
+};
 
-exports.default = series(
-  scssTask,
-  jsTask,
-  browserSyncServe,
-  watchTask
+// Build task for Netlify deployment
+exports.build = series(
+    parallel(htmlTask, scssTask, jsTask)
 );
-// gulp.task('html', () => {
-//   return gulp.src('*.html')
-//     .pipe(concat('index.html'))
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./styles/'));
-// });
+
+
+// Default task sequence: build, serve, and watch
+exports.default = series(
+    parallel(htmlTask, scssTask, jsTask),
+    browserSyncServe,
+    watchTask
+);
